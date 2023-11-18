2b7dae05d337442b8556585e1517ae1957f3cc85
Vinicius <viniciussobreiraborges@hotmail.com>
2023-11-18 12:22:41 -0300

    Ajustado botão de logout

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,28 +1,33 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import {useState} from 'react'
+import {Link} from 'react-router-dom'
+
 
 function Nav() {
-  // Hook - useState
-  const [userLogado] = useState(JSON.parse(sessionStorage.getItem('usuarioLogado')));
+  
+    //Hook -useState
+    const[userLogado]=
+    useState(JSON.parse(sessionStorage.getItem('usuarioLogado')))
 
-  const handleLogout = () => {
-    sessionStorage.removeItem('usuarioLogado');
-    // Atualizar a página para redirecionar para a tela inicial
-    window.location.href = '/login';
-  };
+
+    const handleLogout =()=>{
+        sessionStorage.removeItem("usuarioLogado");
+        window.location.reload();
+    }
 
   return (
     <>
-      <div style={userLogado == null ? { display: 'none' } : { display: 'block' }}>
-        <p className='usuario m-3'>{userLogado != null ? `Usuario Logado: ${userLogado.usuario}` : ''}</p>
-        <button onClick={handleLogout} className='logout m-3'>Logout</button>
-      </div>
 
+    <div style={userLogado == null ? {display:"none"}:{display:"block"}}>
+    <p className='usuario m-3 d-flex justify-content-end'>
+    {userLogado !=null ? `Usuario Logado:${userLogado.usuario}`:""}</p>
 
-      <Link to="/" className='btn-link m-4 text-decoration-none'>Home</Link><br></br>
-      <Link to="/login" className='btn-link m-4 text-decoration-none'>Login</Link>
+    <button onClick={handleLogout} className='logout m-3'>Logout</button>
+    </div>
+    
+    <Link to="/" className='btn-link m-3 text-decoration-none'>Home</Link><br></br> 
+    <Link to="/login" className='btn-link m-3 text-decoration-none'>Login</Link>
     </>
-  );
+  )
 }
 
-export default Nav;
+export default Nav
